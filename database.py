de4b532167e5cbbf810ea700c64498fea895ae2e
hishamfaizal558 <112550357+hishamfaizal558@users.noreply.github.com>
2025-07-12 13:22:47 +0530

    database.py

--- database.py
+++ database.py
@@ -3,7 +3,8 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./collection.db"  # or use PostgreSQL/MySQL
+DATABASE_URL = "DATABASE_URL = "postgresql://collection_mkz6_user:nqYN9Ds0L3uds7DoYRcAC1kONzyKSfJW@dpg-d1p14ak9c44c73845kfg-a.oregon-postgres.render.com/collection_mkz6"
+"  # or use PostgreSQL/MySQL
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
